2019f54d659c170fcd28ccefa8f4f20e0082c7a6
Tyler Helwig <10726803+Fibonacci-@users.noreply.github.com>
2024-10-06 15:02:17 -0400

    add config, autobuild copy

--- PatchIt.cs
+++ PatchIt.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx.Logging;
 using CardSeller;
 using HarmonyLib;
+using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -11,37 +12,12 @@ namespace CardSeller;
 [HarmonyPatch]
 public static class PatchIt
 {
+    private static bool isRunning = false;
 
-    [HarmonyPatch(typeof(InteractableCard3d), "RegisterScanCard")]
-    [HarmonyPostfix]
-    private static void OnRegisterScanCardPostFix(InteractableCard3d __instance)
+    private static void DoShelfPut()
     {
-        Plugin.Logger.LogMessage("Card scanned!");
-        //use this as a trigger to go find it's original slot and replace it?
-        //or iterate through all empty sellable slots
-        Plugin.Logger.LogMessage("Price is " + __instance.GetCurrentPrice());
-    }
-
-    [HarmonyPatch(typeof(CPlayerData), "ReduceCard")]
-    [HarmonyPostfix]
-    private static void OnReduceCardPostFix(CPlayerData __instance)
-    {
-        Plugin.Logger.LogMessage("Reduce card was called!");
-        Plugin.Logger.LogMessage("OK");
-    }
-
-    [HarmonyPatch(typeof(Customer), "TakeCardFromShelf")]
-    [HarmonyPostfix]
-    private static void OnCustomerTakeCardFromShelfPostFix(Customer __instance, List<InteractableCard3d> ___m_CardInBagList)
-    {
-        Plugin.Logger.LogMessage("Customer took a card from the shelf! Filling all empty shelves...");
-        Plugin.Logger.LogMessage(___m_CardInBagList.Count);
-        //List<int> cardList = CPlayerData.GetCardCollectedList(ECardExpansionType.Tetramon, false);
-        //foreach (int cardIndex in cardList)
-        //{
-        //    CardData cardData = CPlayerData.GetCardData(cardIndex, ECardExpansionType.Tetramon, false);
-            
-        //}
+        if(isRunning) return;
+        isRunning = true;
 
         List<CardShelf> cardShelfList = CSingleton<ShelfManager>.Instance.m_CardShelfList;
         foreach (CardShelf shelf in cardShelfList)
@@ -49,32 +25,92 @@ public static class PatchIt
             List<InteractableCardCompartment> cardCompartments = shelf.GetCardCompartmentList();
             foreach (InteractableCardCompartment cardCompart in cardCompartments)
             {
-                if (cardCompart.m_StoredCardList.Count == 0)
+                if (cardCompart.m_StoredCardList.Count == 0 && !cardCompart.m_ItemNotForSale)
                 {
-                    //instantiate card?
+                    //instantiate card
+                    //find items in the CardCollectedList where we have more than 0 in inventory
                     List<int> cardList = CPlayerData.GetCardCollectedList(ECardExpansionType.Tetramon, false);
-                    if (cardList.Count > 0)
+
+                    CardData cardData = null;
+
+                    for (int i = 0; i < cardList.Count; i++)
                     {
-                        CardData cardData = CPlayerData.GetCardData(cardList[0], ECardExpansionType.Tetramon, false);
-                        if (cardData != null && cardData.monsterType != EMonsterType.None)
+                        int numHeldCards = cardList[i];
+                        if (numHeldCards > 0)
                         {
-                            Card3dUIGroup cardUi = CSingleton<Card3dUISpawner>.Instance.GetCardUI();
-                            InteractableCard3d component = ShelfManager.SpawnInteractableObject(EObjectType.Card3d).GetComponent<InteractableCard3d>();
-                            cardUi.m_IgnoreCulling = true;
-                            cardUi.m_CardUI.SetFoilCullListVisibility(true);
-                            cardUi.m_CardUI.ResetFarDistanceCull();
-                            cardUi.m_CardUI.SetCardUI(cardData);
-                            cardUi.transform.position = component.transform.position;
-                            cardUi.transform.rotation = component.transform.rotation;
-                            component.SetCardUIFollow(cardUi);
-                            component.SetEnableCollision(false);
-                            cardCompart.SetCardOnShelf(component);
-                            cardUi.m_IgnoreCulling = false;
-                            CPlayerData.ReduceCard(cardData, 1);
+                            //when we find a held card, grab the card data for it and break out of the loop
+                            CardData insp = CPlayerData.GetCardData(i, ECardExpansionType.Tetramon, false);
+                            Plugin.Logger.LogInfo("Finding held cards: Player has " + numHeldCards + " cards of monster type " + insp.monsterType.ToString());
+                            float currentMP = CPlayerData.GetCardMarketPrice(insp);
+                            if (currentMP > Plugin.m_ConfigSellOnlyGreaterThanMP.Value && currentMP < Plugin.m_ConfigSellOnlyLessThanMP.Value)
+                            {
+                                cardData = insp;
+                                Plugin.Logger.LogInfo("Card price " + currentMP + " meets price restrictions. Continuing to place...");
+                                break;
+                            }
+                            else
+                            {
+                                Plugin.Logger.LogInfo("Card price is outside price restriction band (" + currentMP + "). Continuing search...");
+                            }
+
                         }
                     }
+
+                    if (cardData != null && cardData.monsterType != EMonsterType.None)
+                    {
+                        //ref card shelf load from save method
+                        Plugin.Logger.LogInfo("Initing game object for " + cardData.monsterType.ToString());
+                        Card3dUIGroup cardUi = CSingleton<Card3dUISpawner>.Instance.GetCardUI();
+                        InteractableCard3d component = ShelfManager.SpawnInteractableObject(EObjectType.Card3d).GetComponent<InteractableCard3d>();
+                        cardUi.m_IgnoreCulling = true;
+                        cardUi.m_CardUI.SetFoilCullListVisibility(true);
+                        cardUi.m_CardUI.ResetFarDistanceCull();
+                        cardUi.m_CardUI.SetCardUI(cardData);
+                        cardUi.transform.position = component.transform.position;
+                        cardUi.transform.rotation = component.transform.rotation;
+                        component.SetCardUIFollow(cardUi);
+                        component.SetEnableCollision(false);
+                        cardCompart.SetCardOnShelf(component);
+                        cardUi.m_IgnoreCulling = false;
+                        Plugin.Logger.LogInfo("Reducing held card count by 1 for monster " + cardData.monsterType.ToString());
+                        CPlayerData.ReduceCard(cardData, 1);
+                    }
+
                 }
             }
+        }
+        isRunning = false;
+    }
+
+    [HarmonyPatch(typeof(CPlayerData), "ReduceCard")]
+    [HarmonyPostfix]
+    private static void OnReduceCardPostFix(CPlayerData __instance, CardData cardData, int reduceAmount)
+    {
+        Plugin.Logger.LogInfo("Reduce card was called!");
+
+        Plugin.Logger.LogMessage("Reduced " + cardData.monsterType.ToString() + " by " + reduceAmount);
+        int cardIndex = CPlayerData.GetCardSaveIndex(cardData);
+        int heldCards = CPlayerData.GetCardAmountByIndex(cardIndex, ECardExpansionType.Tetramon, false);
+        Plugin.Logger.LogMessage("Player card amount for monster " + cardData.monsterType.ToString() + " with index " + cardIndex + " is " + heldCards + " num cards.");
+    }
+
+    [HarmonyPatch(typeof(Customer), "TakeCardFromShelf")]
+    [HarmonyPostfix]
+    private static void OnCustomerTakeCardFromShelfPostFix(Customer __instance, List<InteractableCard3d> ___m_CardInBagList)
+    {
+        Plugin.Logger.LogInfo("Customer took a card from the shelf! Filling all empty card sale shelves...");
+
+        DoShelfPut();
+    }
+
+    [HarmonyPatch(typeof(CGameManager), "Update")]
+    [HarmonyPostfix]
+    public static void OnGameManagerUpdatePostfix()
+    {
+        if (Plugin.m_ConfigKeyboardTriggerCardSet.Value.IsDown())
+        {
+            Plugin.Logger.LogInfo("Shortcut key was pressed!");
+            DoShelfPut();
         }
     }
 
--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,10 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using BepInExPlugin;
 using HarmonyLib;
+using System;
+using UnityEngine;
 
 namespace CardSeller;
 
@@ -11,12 +14,35 @@ public class Plugin : BaseUnityPlugin
     internal static new ManualLogSource Logger;
 
     private readonly Harmony harmony = new Harmony(CSInfo.PLUGIN_NAME);
+
+    //internal static float SellOnlyGreaterThanMP = 0.50f;
+    //internal static float SellOnlyLessThanMP = 100.00f;
+    internal static ConfigEntry<float> m_ConfigSellOnlyGreaterThanMP;
+    internal static ConfigEntry<float> m_ConfigSellOnlyLessThanMP;
+    internal static ConfigEntry<KeyboardShortcut> m_ConfigKeyboardTriggerCardSet;
+
+    //private ConfigEntry<bool> m_SetLowerPriceLimit;
+    //private ConfigEntry<bool> m_SetUpperPriceLimit;
+
+    private void InitConfig()
+    {
+        m_ConfigSellOnlyGreaterThanMP = Config.Bind("General", "SellOnlyGreaterThan", 0.50f, "Ignore cards in the album with a market value below this.");
+        m_ConfigSellOnlyLessThanMP = Config.Bind("General", "SellOnlyLessThan", 100.00f, "Ignore cards in the album with a market value above this.");
+        m_ConfigKeyboardTriggerCardSet = Config.Bind<KeyboardShortcut>("General", "SetOutCards", new KeyboardShortcut(KeyCode.F9, Array.Empty<KeyCode>()), "Keyboard shortcut to set out cards.");
+    }
         
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
+        InitConfig();
         this.harmony.PatchAll();
         Logger.LogInfo($"Plugin {CSInfo.PLUGIN_ID} is loaded!");
+    }
+
+    private void OnDestroy()
+    {
+        this.harmony?.UnpatchSelf();
+        Logger.LogInfo($"Plugin {CSInfo.PLUGIN_ID} is unloaded!");
     }
 }
